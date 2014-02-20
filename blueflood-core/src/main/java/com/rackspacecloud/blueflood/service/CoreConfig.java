a0e57acb63494b6143f1ea2df5a0c2cd8d8790e7
Chinmay Gupte <chinmay.gupte@rackspace.com>
2014-02-20 15:48:11 -0800

    Schedule slots for rollup only within catch-up period

--- blueflood-core/src/main/java/com/rackspacecloud/blueflood/service/CoreConfig.java
+++ blueflood-core/src/main/java/com/rackspacecloud/blueflood/service/CoreConfig.java
@@ -128,7 +128,10 @@ public enum CoreConfig implements ConfigDefaults {
     METADATA_CACHE_PERSISTENCE_ENABLED("false"),
     METADATA_CACHE_PERSISTENCE_PATH("/dev/null"),
     METADATA_CACHE_PERSISTENCE_PERIOD_MINS("10"),
-    META_CACHE_RETENTION_IN_MINUTES("10");
+    META_CACHE_RETENTION_IN_MINUTES("10"),
+
+    //Catch period in hours
+    CATCH_PERIOD("1");
 
     static {
         Configuration.getInstance().loadDefaults(CoreConfig.values());
